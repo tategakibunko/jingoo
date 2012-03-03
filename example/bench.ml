64ff74e629c1f49b3850c9106982b1f92636ac6e
tategakibunko <lambda.watanabe@gmail.com>
2012-03-03 12:30:38 +0900

    add cmdline option '-count' for benchmark tool

--- example/bench.ml
+++ example/bench.ml
@@ -3,20 +3,25 @@ open Jg_utils
 
 let compiled = ref false
 let file = ref "cheatsheet.tmpl"
+let count = ref 1000
   
 let () =
   Arg.parse [
     ("-cmpl", Arg.Unit (fun () -> compiled := true), "use compiled template");
     ("-tmpl", Arg.String (fun f -> file := f), "template name");
+    ("-count", Arg.Int (fun i -> count := max 5 i), "loop count");
   ] ignore "";
 
   let output () = 
     ignore @@ Jg_template.from_file !file ~use_compiled:(!compiled) ~models:Test_data.models in
 
+  let t0 = Unix.gettimeofday () in
   for i = 1 to 5 do
-    for j = 1 to 200 do
+    for j = 1 to (!count / 5) do
       output ()
     done;
-    print_endline @@ spf "%d/5 done" i
-  done
+    Printf.printf "%d/5 done\n%!" i
+  done;
+  let t1 = Unix.gettimeofday () in
+  Printf.printf "time: %f\n" (t1 -. t0)
 ;;
