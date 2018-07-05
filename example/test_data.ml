3012e6fa24addeb4034a8c77980b4499b66ae8f0
tategakibunko <lambda.watanabe@gmail.com>
2018-07-05 10:59:49 +0100

    Fix invalid call args of slice in example template

--- example/test_data.ml
+++ example/test_data.ml
@@ -10,7 +10,7 @@ let models = [
   ("msg", Tstr "hello world");
   ("list1", Tlist [Tint 1]);
   ("list", Tlist [Tint 10; Tint 20; Tint 30]);
-  ("long_list", Tlist [Tint 10; Tint 20; Tint 30; Tint 40; Tint 50;	Tint 60; Tint 70; Tint 80; Tint 90; Tint 100]);
+  ("long_list", Tlist [Tint 10; Tint 20; Tint 30; Tint 40; Tint 50; Tint 60; Tint 70; Tint 80; Tint 90; Tint 100]);
   ("obj", Tobj [("name", Tstr "hoge"); ("age", Tint 10)]);
   ("obj2", Tobj [
     ("child", Tobj [
