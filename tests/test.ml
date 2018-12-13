1146b95b7ff8e2b9bb84132698b909f99e191b05
Julien Sagot <julien.sagot@geneanet.org>
2018-12-13 07:35:53 +0100

    Added tests for dead code elimination. Added ppx_deriving.show for test printers.

--- tests/test.ml
+++ tests/test.ml
@@ -1,5 +1,7 @@
 open OUnit2
 
 let () =
-  run_test_tt_main ("jingoo" >::: [Test_runtime.suite; Test_output.suite])
-;;
+  run_test_tt_main ("jingoo" >::: [ Test_runtime.suite
+                                  ; Test_output.suite
+                                  ; Test_dead_code_elimination.suite
+                                  ])
---
+++ tests/test_dead_code_elimination.ml
@@ -0,0 +1,71 @@
+open OUnit2
+open Jingoo
+open Jg_types
+
+let assert_eq expected source =
+  let lexbuf = Lexing.from_string source in
+  Jg_lexer.reset_context () ;
+  Jg_lexer.init_lexer_pos None lexbuf ;
+  let ast =
+    try
+      Jg_parser.input Jg_lexer.main lexbuf
+      |> Jg_interp.dead_code_elimination
+    with e -> failwith (Jg_utils.get_parser_error e lexbuf)
+  in
+  assert_equal ~printer:Jg_types.show_ast expected ast
+
+let test_0 _ =
+  assert_eq
+    [ FunctionStatement ( IdentExpr "foo"
+                        , []
+                        , [ ExpandStatement (LiteralExpr (Tint 42))
+                          ] )
+    ; ExpandStatement (ApplyExpr (IdentExpr "foo", []) )
+    ]
+    "{% function foo () %}{{ 42 }}{% endfunction %}\
+    {{ foo () }}"
+
+let test_1 _ =
+  assert_eq
+    [ Statements [] ]
+    "{% function foo () %}{{ 42 }}{% endfunction %}"
+
+let test_2 _ =
+  assert_eq
+    [ Statements []
+    ; Statements []
+    ; Statements [] ]
+    "{% function foo () %}{{ foo () }}{% endfunction %}\
+     {% function bar () %}{{ baz () }}{% endfunction %}\
+     {% function baz () %}{{ bar () }}{% endfunction %}\
+    "
+
+let test_3 _ =
+  assert_eq
+    [ Statements []
+    ; ForStatement ( SetExpr [ IdentExpr "foo" ]
+                   , IdentExpr "bar"
+                   , [ ExpandStatement (ApplyExpr (IdentExpr "foo", []) ) ])
+    ]
+    "{% function foo () %}{{ 42 }}{% endfunction %}\
+     {% for foo in bar %}{{ foo () }}{% endfor %}\
+    "
+
+let test_4 _ =
+  assert_eq
+    [ Statements []
+    ; SetStatement ( SetExpr [ IdentExpr "foo" ], LiteralExpr (Tint 42))
+    ; ExpandStatement (IdentExpr "foo")
+    ]
+    "{% function foo () %}{{ 42 }}{% endfunction %}\
+     {% set foo = 42 %}\
+     {{ foo }}\
+    "
+
+let suite = "runtime test" >::: [
+  "test_1" >:: test_1;
+  "test_0" >:: test_0;
+  "test_2" >:: test_2;
+  "test_3" >:: test_3;
+  "test_4" >:: test_4;
+]
