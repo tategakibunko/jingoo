b6cb89735ecf7ac57a0db1854b4e2310ea394e58
tategakibunko <lambda.watanabe@gmail.com>
2018-12-17 12:14:10 +0000

    Added escape for single quote(U+0027)

--- src/jg_utils.ml
+++ src/jg_utils.ml
@@ -143,14 +143,14 @@ let rec substring base count str =
   else
     UTF8.sub str base count
 
-(** [escape_html str] replaces '&', '"', '<' and '>'
+(** [escape_html str] replaces '&', '"', '\'', '<' and '>'
     with their corresponding character entities (using entity number) *)
 let escape_html str =
   let strlen = String.length str in
   let rec loop acc i =
     if i < strlen then
       match String.unsafe_get str i with
-      | '&' | '"' | '<' | '>' -> loop (acc + 5) (i + 1) (* "&#xx;" *)
+      | '&' | '"' | '\'' | '<' | '>' -> loop (acc + 5) (i + 1) (* "&#xx;" *)
       | _ -> loop (acc + 1) (i + 1)
     else if acc = strlen then str
     else
@@ -158,10 +158,11 @@ let escape_html str =
       let rec loop istr ibuf =
         if istr = strlen then Bytes.unsafe_to_string buf
         else match String.unsafe_get str istr with
-          | '&' -> Bytes.blit_string "&#38;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
-          | '"' -> Bytes.blit_string "&#34;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
-          | '<' -> Bytes.blit_string "&#60;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
-          | '>' -> Bytes.blit_string "&#62;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
+          | '&'  -> Bytes.blit_string "&#38;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
+          | '"'  -> Bytes.blit_string "&#34;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
+          | '\'' -> Bytes.blit_string "&#39;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
+          | '<'  -> Bytes.blit_string "&#60;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
+          | '>'  -> Bytes.blit_string "&#62;" 0 buf ibuf 5 ; loop (istr + 1) (ibuf + 5)
           | c -> Bytes.unsafe_set buf ibuf c ; loop (istr + 1) (ibuf + 1)
       in loop 0 0
   in
