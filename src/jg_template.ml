7426b2e55859bb96e1102f155d57d348d4a40c8e
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2020-04-07 21:40:11 +0100

    Interface for loading templates for multiple evaluations.
    Add a new module to the Jg_template interface which allows one to
    store a parsed template in memory so it can be evaluated multiple
    times against different models more efficiently.

--- src/jg_template.ml
+++ src/jg_template.ml
@@ -32,3 +32,17 @@ let from_chan = content (Jg_interp.from_chan ?file_path:None)
 let from_file = content Jg_interp.from_file
 
 let from_string = content (Jg_interp.from_string ?file_path:None)
+
+module Loaded = struct
+  type t = Jg_interp.Loaded.t
+
+  let from_chan ?env chan = Jg_interp.Loaded.from_chan ?env chan
+  let from_file ?env file_name = Jg_interp.Loaded.from_file ?env file_name
+  let from_string ?env source = Jg_interp.Loaded.from_string ?env source
+
+  let eval ?ctx ?(models=[]) t =
+    let buffer = Buffer.create 1024 in
+    let output x = Buffer.add_string buffer (Jg_runtime.string_of_tvalue x) in
+    let () = Jg_interp.Loaded.eval t ~models ~output ?ctx () in
+    Buffer.contents buffer
+end
